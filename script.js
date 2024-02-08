533c3569d0adfcf0f4ed0e8dad8ffbd7099e49c3
MD. IMTIAZ KHONDOKAR SUPTO <158520611+mdiks23@users.noreply.github.com>
2024-02-08 21:48:04 +0600

    Update script.js
    Testing

--- script.js
+++ script.js
@@ -153,4 +153,14 @@ const isLeapYear = (year) => {
     )}: ${`${timer.getSeconds()}`.padStart(2, '0')}`;
     todayShowTime.textContent = formateTimer;
   }, 1000);
-  
+  const stars = document.querySelectorAll("#star");
+
+stars.forEach((star) => {
+  let duration = Math.random() * (1.2 - 0.6) + 0.6;
+  star.style.animation = `stars ${duration}s infinite linear`;
+});
+
+window.addEventListener("load", () => {
+  const preloader = document.querySelector(".preloader");
+  preloader.classList.add("preload-finish");
+});
